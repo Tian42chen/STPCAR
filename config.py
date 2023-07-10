92153db115323618ef9638ac27e3eca15d0590b6
Tian_chen <90170798+Tian42chen@users.noreply.github.com>
2023-07-10 09:02:44 +0800

    [NEW] some train logs

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ learning_rate = 0.01
 momentum = 0.9
 weight_decay = 1e-4
 batch_size = 16
-num_epochs = 30
-print_interval=10
-# num_epochs = 50
-# print_interval = 100
+# num_epochs = 30
+# print_interval=10
+num_epochs = 50
+print_interval = 100
 workers=4
 
-lr_milestones=[12, 18]
-# lr_milestones=[20, 30]
+# lr_milestones=[12, 18]
+lr_milestones=[20, 30]
 lr_gamma=0.1
 lr_warmup_epochs=10
 
 # Data paths
-data_path = './data/raw/testpcd/'
+data_path = './data/raw/pcd/'
 
 # Model paths
 save_model_path = './checkpoints/'
